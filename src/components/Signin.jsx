413ca28e1c696357cb261fadbf52f01b7dccd8c8
runefar1 <rune@norderhaug.name>
2019-07-27 15:10:59 -0700

    Remove legacy header

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -10,7 +10,6 @@ export default class Signin extends Component {
 
     return (
       <div className="panel-landing" id="section-1">
-        <h1 className="landing-heading">Hello, Blockstack!</h1>
         <p className="lead">
           <button
             className="btn btn-primary btn-lg"
