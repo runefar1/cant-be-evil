d95e90d573fe434eb9baf4fa48445b1492693e12
runefar1 <rune@norderhaug.name>
2019-07-30 16:15:57 -0700

    Add login design

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,8 +3,11 @@ import Profile from './Profile.jsx';
 import Signin from './Signin.jsx';
 import {
   UserSession,
-  AppConfig
+  AppConfig,
+  Person
 } from 'blockstack';
+
+const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
 const appConfig = new AppConfig()
 const userSession = new UserSession({ appConfig: appConfig })
@@ -13,7 +16,18 @@ export default class Auth extends Component {
 
   constructor(props) {
   	super(props);
-  }
+    this.state = {
+        person: {
+          name() {
+              return 'Anonymous';
+            },
+          avatarUrl(){
+            return avatarFallbackImage;
+            }
+          }
+        }
+    }
+
 
   handleSignIn(e) {
     e.preventDefault();
@@ -26,8 +40,18 @@ export default class Auth extends Component {
   }
 
   render() {
+    const {person} = this.state;
     return (
       <div className ="Auth">
+          { userSession.isUserSignedIn() ?
+            <span className= "avatar">
+
+                <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage}
+                    className = "avatar-image" id="avatar-image" />
+                { person.name() }
+            </span>
+            : null }
+
           { !userSession.isUserSignedIn() ?
             <button
               className="btn btn-primary"
@@ -36,7 +60,7 @@ export default class Auth extends Component {
               Sign In
             </button>
             : <button
-              className="btn btn-secondary"
+              className="btn btn-outline-secondary"
               onClick={ this.handleSignOut }
             >
               Sign Out
@@ -50,6 +74,8 @@ export default class Auth extends Component {
     if (userSession.isSignInPending()) {
       userSession.handlePendingSignIn().then((userData) => {
         window.location = window.location.origin;
+        this.setState({person: new Person(userData.profile)
+        })
       });
     }
   }
