ee0c335439fddc71ce54d980658db8fe370974f8
Terje Norderhaug <terje@in-progress.com>
2019-08-02 00:29:54 -0700

    Keep visit history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,13 @@ import Profile from './Profile.jsx';
 import Signin from './Signin.jsx';
 import { userSession } from './Global.js';
 
+const settingsFilename = "settings_1"
+
 export default class App extends Component {
 
   constructor(props) {
   	super(props);
+    this.state = {}
   }
 
   handleSignIn(e) {
@@ -34,11 +37,34 @@ export default class App extends Component {
     );
   }
 
+  uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+  }
+
+  setVisitCount(n) {
+    this.setState({visitCount: n})
+    var style = document.createElement('style');
+    document.head.appendChild(style);
+    style.sheet.insertRule('.app-visit-count::before {content:"' + n + '"}');
+  }
+
   handleSignedIn (userData) {
     // window.location = window.location.origin;
     console.log("User Signed In")
+
+    const historyFile = "history/" + this.uuidv4()
+    const timeStamp = "" + Date.now()
+    userSession.putFile(historyFile, JSON.stringify({when: timeStamp}))
+
     if (this){
-      this.setState({userData: userData})
+      this.setState({userData: userData,
+                     historyFile: historyFile})
+      userSession.listFiles(file => true)
+      .then(this.setVisitCount.bind(this))
+      .catch( err => console.log("Failed to count visits:", err))
+      .finally(value => console.log("Visits:", value))
     }
     document.documentElement.className += "user-signed-in"
   }
